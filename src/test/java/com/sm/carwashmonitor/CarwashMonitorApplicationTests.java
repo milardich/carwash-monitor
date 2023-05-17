99a49f6db22a0c51663d29ae143fa51118f0d578
milardich <stjepan.milardic1@gmail.com>
2023-05-17 16:42:05 +0200

    spring boot init

---
+++ src/test/java/com/sm/carwashmonitor/CarwashMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.carwashmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarwashMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
