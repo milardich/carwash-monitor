fff80b2c59f5129dfbfc8b50b961132332d89ada
milardich <stjepan.milardic1@gmail.com>
2024-03-29 21:51:40 +0100

    added UnitCard component, improved styles

---
+++ src/components/UnitCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps<{
+    state: string
+}>()
+
+var cssClass: String = "bg-yellow-warning";
+
+switch (props.state) {
+    case "AVAILABLE": cssClass = "bg-green-light";
+        break
+    case "IN USE": cssClass = "bg-red-light";
+        break
+    case "INACTIVE": cssClass = "bg-yellow-warning"
+        break
+}
+</script>
+
+<template>
+    <div
+        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between bg-violet-ultralight">
+        <div class="flex">
+            <div class="text-3xl">#1</div>
+            <div class="ml-auto justify-end">
+                <button>
+                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset="" class="card-button-icon-small">
+                </button>
+            </div>
+        </div>
+
+        <div class="mt-2">
+            <div class="grid grid-cols-2 text-sm">
+                <div>Washes today:</div>
+                <div class="font-bold">12</div>
+                <div>Coin tray:</div>
+                <div class="font-bold">69%</div>
+            </div>
+        </div>
+
+        <div class="border-1 border-black rounded-lg text-center p-1" :class="cssClass">
+            {{ state }}
+        </div>
+    </div>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import StationDropdown from '@/components/StationDropdown.vue';
+import UnitCard from '@/components/UnitCard.vue';
 </script>
 
 
@@ -10,140 +11,38 @@ import StationDropdown from '@/components/StationDropdown.vue';
         </div>
 
         <div class="grid grid-cols-2 gap-4 h-percent-90 mt-5">
-            <div class="rounded-lg overflow-y-auto p-6 h-full bg-white-light shadow-lg">
+            <div class="rounded-lg overflow-y-auto p-6 h-full  bg-white-medium shadow-lg">
                 <div class="text-3xl">Resource consumption</div>
 
                 <!-- TODO: make this a component -->
                 <div>
-                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-10 shadow-lg bg-white-light">
                         Resource consumption graph 1
                     </div>
-                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-10 shadow-lg bg-white-light">
                         Resource consumption graph 1
                     </div>
-                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-10 shadow-lg bg-white-light">
                         Resource consumption graph 1
                     </div>
-                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-10 shadow-lg bg-white-light">
                         Resource consumption graph 1
                     </div>
                 </div>
             </div>
 
-            <div class="rounded-lg overflow-y-auto p-6 h-full bg-white-light shadow-lg">
+            <div class="rounded-lg overflow-y-auto p-6 h-full bg-white-medium shadow-lg">
                 <div class="text-3xl">Units</div>
 
                 <!-- TODO: make this a component -->
                 <div class="grid grid-cols-3 gap-4 mt-6">
-
-                    <!-- unit #1 -->
-                    <div
-                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
-                        <div class="flex">
-                            <div class="text-3xl">#1</div>
-                            <div class="ml-auto justify-end">
-                                <button>
-                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
-                                        class="card-button-icon-small">
-                                </button>
-                            </div>
-                        </div>
-
-                        <div class="mt-2">
-                            <div class="grid grid-cols-2 text-sm">
-                                <div>Washes today:</div>
-                                <div class="font-bold">12</div>
-                                <div>Coin tray:</div>
-                                <div class="font-bold">69%</div>
-                            </div>
-                        </div>
-
-                        <div class="border-1 border-black rounded-lg text-center bg-yellow-warning p-1">
-                            INACTIVE
-                        </div>
-                    </div>
-
-                    <!-- unit #1 -->
-                    <div
-                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
-                        <div class="flex">
-                            <div class="text-3xl">#2</div>
-                            <div class="ml-auto justify-end">
-                                <button>
-                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
-                                        class="card-button-icon-small">
-                                </button>
-                            </div>
-                        </div>
-
-                        <div class="mt-2">
-                            <div class="grid grid-cols-2 text-sm">
-                                <div>Washes today:</div>
-                                <div class="font-bold">12</div>
-                                <div>Coin tray:</div>
-                                <div class="font-bold">69%</div>
-                            </div>
-                        </div>
-
-                        <div class="border-1 border-black rounded-lg text-center bg-green-light p-1">
-                            AVAILABLE
-                        </div>
-                    </div>
-
-                    <!-- unit #1 -->
-                    <div
-                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
-                        <div class="flex">
-                            <div class="text-3xl">#3</div>
-                            <div class="ml-auto justify-end">
-                                <button>
-                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
-                                        class="card-button-icon-small">
-                                </button>
-                            </div>
-                        </div>
-
-                        <div class="mt-2">
-                            <div class="grid grid-cols-2 text-sm">
-                                <div>Washes today:</div>
-                                <div class="font-bold">12</div>
-                                <div>Coin tray:</div>
-                                <div class="font-bold">69%</div>
-                            </div>
-                        </div>
-
-                        <div class="border-1 border-black rounded-lg text-center bg-red-light p-1">
-                            IN USE
-                        </div>
-                    </div>
-
-                    <!-- unit #1 -->
-                    <div
-                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
-                        <div class="flex">
-                            <div class="text-3xl">#4</div>
-                            <div class="ml-auto justify-end">
-                                <button>
-                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
-                                        class="card-button-icon-small">
-                                </button>
-                            </div>
-                        </div>
-
-                        <div class="mt-2">
-                            <div class="grid grid-cols-2 text-sm">
-                                <div>Washes today:</div>
-                                <div class="font-bold">12</div>
-                                <div>Coin tray:</div>
-                                <div class="font-bold">69%</div>
-                            </div>
-                        </div>
-
-                        <div class="border-1 border-black rounded-lg text-center bg-yellow-warning p-1">
-                            INACTIVE
-                        </div>
-                    </div>
-
+                    <UnitCard state="AVAILABLE" />
+                    <UnitCard state="IN USE" />
+                    <UnitCard state="IN USE" />
+                    <UnitCard state="AVAILABLE" />
+                    <UnitCard state="INACTIVE" />
+                    <UnitCard state="INACTIVE" />
+                    <UnitCard state="AVAILABLE" />
                 </div>
             </div>
 
