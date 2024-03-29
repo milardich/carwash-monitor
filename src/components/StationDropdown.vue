862d9419039dcf2c23ccb8551af396759e7bdd2b
milardich <stjepan.milardic1@gmail.com>
2024-03-29 20:17:23 +0100

    unit cards css

--- src/components/StationDropdown.vue
+++ src/components/StationDropdown.vue
@@ -1,8 +1,11 @@
 <template>
     <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" @click="toggleStationDropdown"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button">Station 1<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-            fill="none" viewBox="0 0 10 6">
+        class="px-5 py-2.5 text-center inline-flex bg-violet-light text-white-light rounded-lg text-xl" type="button">
+
+        Station 1
+
+        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="m1 1 4 4 4-4" />
         </svg>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -9,57 +9,146 @@ import StationDropdown from '@/components/StationDropdown.vue';
             <StationDropdown />
         </div>
 
-        <div class="grid grid-cols-2 gap-4 border-2 border-green h-percent-90 mt-10">
-            <div class="rounded-lg border-2 border-black overflow-y-auto p-6 h-full">
-                <div class="text-lg">Resource consumption</div>
+        <div class="grid grid-cols-2 gap-4 h-percent-90 mt-5">
+            <div class="rounded-lg overflow-y-auto p-6 h-full bg-white-light shadow-lg">
+                <div class="text-3xl">Resource consumption</div>
 
                 <!-- TODO: make this a component -->
                 <div>
-                    <div class="rounded-lg border-2 border-black h-64 mt-6">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
                         Resource consumption graph 1
                     </div>
-                    <div class="rounded-lg border-2 border-black h-64 mt-6">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
                         Resource consumption graph 1
                     </div>
-                    <div class="rounded-lg border-2 border-black h-64 mt-6">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
                         Resource consumption graph 1
                     </div>
-                    <div class="rounded-lg border-2 border-black h-64 mt-6">
+                    <div class="rounded-2xl border-1 border-black h-64 mt-6 shadow-lg">
                         Resource consumption graph 1
                     </div>
                 </div>
             </div>
 
-            <div class="rounded-lg border-2 border-black overflow-y-auto p-6 h-full">
-                <div class="text-lg">Units</div>
+            <div class="rounded-lg overflow-y-auto p-6 h-full bg-white-light shadow-lg">
+                <div class="text-3xl">Units</div>
 
                 <!-- TODO: make this a component -->
                 <div class="grid grid-cols-3 gap-4 mt-6">
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                    <div class="box-border h-48 w-56 rounded-lg border-2 border-black">
-                        Unit 1
-                    </div>
-                </div>
 
+                    <!-- unit #1 -->
+                    <div
+                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
+                        <div class="flex">
+                            <div class="text-3xl">#1</div>
+                            <div class="ml-auto justify-end">
+                                <button>
+                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
+                                        class="card-button-icon-small">
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="mt-2">
+                            <div class="grid grid-cols-2 text-sm">
+                                <div>Washes today:</div>
+                                <div class="font-bold">12</div>
+                                <div>Coin tray:</div>
+                                <div class="font-bold">69%</div>
+                            </div>
+                        </div>
+
+                        <div class="border-1 border-black rounded-lg text-center bg-yellow-warning p-1">
+                            INACTIVE
+                        </div>
+                    </div>
+
+                    <!-- unit #1 -->
+                    <div
+                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
+                        <div class="flex">
+                            <div class="text-3xl">#2</div>
+                            <div class="ml-auto justify-end">
+                                <button>
+                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
+                                        class="card-button-icon-small">
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="mt-2">
+                            <div class="grid grid-cols-2 text-sm">
+                                <div>Washes today:</div>
+                                <div class="font-bold">12</div>
+                                <div>Coin tray:</div>
+                                <div class="font-bold">69%</div>
+                            </div>
+                        </div>
+
+                        <div class="border-1 border-black rounded-lg text-center bg-green-light p-1">
+                            AVAILABLE
+                        </div>
+                    </div>
+
+                    <!-- unit #1 -->
+                    <div
+                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
+                        <div class="flex">
+                            <div class="text-3xl">#3</div>
+                            <div class="ml-auto justify-end">
+                                <button>
+                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
+                                        class="card-button-icon-small">
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="mt-2">
+                            <div class="grid grid-cols-2 text-sm">
+                                <div>Washes today:</div>
+                                <div class="font-bold">12</div>
+                                <div>Coin tray:</div>
+                                <div class="font-bold">69%</div>
+                            </div>
+                        </div>
+
+                        <div class="border-1 border-black rounded-lg text-center bg-red-light p-1">
+                            IN USE
+                        </div>
+                    </div>
+
+                    <!-- unit #1 -->
+                    <div
+                        class="box-border h-48 w-56 grid grid-cols-1 rounded-2xl border-1 border-black shadow-lg p-4 content-between">
+                        <div class="flex">
+                            <div class="text-3xl">#4</div>
+                            <div class="ml-auto justify-end">
+                                <button>
+                                    <img src="@/assets/settings-svgrepo-com.svg" alt="" srcset=""
+                                        class="card-button-icon-small">
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="mt-2">
+                            <div class="grid grid-cols-2 text-sm">
+                                <div>Washes today:</div>
+                                <div class="font-bold">12</div>
+                                <div>Coin tray:</div>
+                                <div class="font-bold">69%</div>
+                            </div>
+                        </div>
+
+                        <div class="border-1 border-black rounded-lg text-center bg-yellow-warning p-1">
+                            INACTIVE
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
         </div>
     </div>
+
 
 </template>
