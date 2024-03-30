a382200cdb41c11d68c85ffa288e0ef5fed049f5
milardich <stjepan.milardic1@gmail.com>
2024-03-30 15:25:30 +0100

    added colored line chart

--- src/components/ResourceChartCard.vue
+++ src/components/ResourceChartCard.vue
@@ -1,42 +1,43 @@
 <script setup lang="ts">
-import { Bar } from 'vue-chartjs';
+import { Line } from 'vue-chartjs';
 </script>
 
 <template>
     <div class="rounded-2xl border-1 border-black h-64 mt-10 shadow-lg bg-white-light">
-        Resource consumption chart 1
-        <Bar :data="data" :options="options" />
+        <Line :data="data" :options="options" />
     </div>
 </template>
 
 <script lang="ts">
 import {
     Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
     Title,
     Tooltip,
-    Legend,
-    BarElement,
-    CategoryScale,
-    LinearScale
+    Legend
 } from 'chart.js'
+import * as chartConfig from '@/components/chartConfig.js'
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
 
 export default {
     name: 'App',
     components: {
-        Bar
+        Line
     },
     data() {
-        return {
-            data: {
-                labels: ['January', 'February', 'March'],
-                datasets: [{ data: [40, 20, 12] }]
-            },
-            options: {
-                responsive: true
-            }
-        }
+        return chartConfig
     }
 }
 </script>
