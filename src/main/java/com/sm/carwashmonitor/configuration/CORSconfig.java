58b863f144fba303c6b53507ad0aa69f985e207e
milardich <stjepan.milardic1@gmail.com>
2024-04-04 00:24:15 +0200

    changed CORS config to allow all methods

--- src/main/java/com/sm/carwashmonitor/configuration/CORSconfig.java
+++ src/main/java/com/sm/carwashmonitor/configuration/CORSconfig.java
@@ -5,19 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class CORSconfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(httpSecurityCorsConfigurer ->
-                httpSecurityCorsConfigurer.configurationSource(request ->
-                        new CorsConfiguration().applyPermitDefaultValues()
-                )
-        );
+        http.cors().and().csrf().disable();
         return http.build();
     }
 }
