fa5de871fe3fc2f08f7708821c24b3fac4f02aeb
milardich <stjepan.milardic1@gmail.com>
2024-04-09 21:22:07 +0200

    edited unit popup style

--- src/components/UnitPopup.vue
+++ src/components/UnitPopup.vue
@@ -6,14 +6,14 @@ import { unitPopup, toggleUnitPopup } from '@/stores/unitPopup';
 <template>
     <!-- Main modal -->
     <div id="default-modal" :class="{ 'hidden': !unitPopup.isOpen }" tabindex="-1"
-        class="flex justify-center items-center h-screen overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 backdrop-blur-sm z-50">
+        class="flex justify-center items-center h-screen overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 backdrop-blur-sm z-50 bg-transparent-black">
         <!-- Popup -->
-        <div class="border-red absolute p-4 w-full max-w-2xl max-h-full">
+        <div class="absolute p-2 w-full max-w-2xl max-h-full">
             <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 bg-white-light p-4">
                 <!-- Modal header -->
-                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                <div class="flex items-center justify-between rounded-t dark:border-gray-600">
+                    <h3 class="text-2xl font-semibold dark:text-white">
                         Unit #{{ unitPopup.selectedUnit?.unitId }}
                     </h3>
                     <button type="button"
@@ -27,26 +27,30 @@ import { unitPopup, toggleUnitPopup } from '@/stores/unitPopup';
                         <span class="sr-only">Close modal</span>
                     </button>
                 </div>
+
+
                 <!-- Modal body -->
-                <div class="p-4 md:p-5 space-y-4">
-                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        With less than a month to go before the European Union enacts new consumer privacy laws for its
-                        citizens, companies around the world are updating their terms of service agreements to comply.
-                    </p>
-                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
-                        and is meant to ensure a common set of data rights in the European Union. It requires
-                        organizations to notify users as soon as possible of high-risk data breaches that could
-                        personally affect them.
-                    </p>
+                <div class="flex mt-6">
+                    <div>
+                        Filter by:
+                    </div>
+                    <div class="ml-6">
+                        Today <!-- TODO: make this a dropdown -->
+                    </div>
                 </div>
+
+                <div class="mt-6">
+                    <div>Number of washes: 12</div> <!-- TODO: -->
+                    <div>Coin tray: {{ unitPopup.selectedUnit?.coinTrayAmount }}</div>
+                    <div>Water consumption: {{ unitPopup.selectedUnit?.totalWaterConsumption }}</div>
+                    <div>Detergent consumption: {{ unitPopup.selectedUnit?.totalDetergentConsumption }}</div>
+                    <div>Wax consumption: {{ unitPopup.selectedUnit?.totalWaxConsumption }}</div>
+                </div>
+
+
                 <!-- Modal footer -->
-                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                    <button data-modal-hide="default-modal" type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                        accept</button>
-                    <button data-modal-hide="default-modal" type="button"
-                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                <div class="mt-6">
+                    TODO: change status button
                 </div>
             </div>
         </div>
