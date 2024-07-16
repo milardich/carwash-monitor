b61b366df4d907e07a97b36c431b2eb9938e3e8a
milardich <stjepan.milardic1@gmail.com>
2024-07-17 01:20:41 +0200

    added init_db.sh

---
+++ init_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+:: Create the database, connect to it
+docker exec -it carwash_db-postgres-1 psql -U postgres -c "CREATE DATABASE carwash_db;"
+docker exec -it carwash_db-postgres-1 psql -U postgres -c "\c carwash_db"
+
+:: Copy sql script to docker container and execute (Create tables and fill with dummy data)
+docker cp dumped_data_carwash.sql carwash_db-postgres-1:/dumped_data_carwash.sql
+docker exec -it carwash_db-postgres-1 psql -U postgres -d carwash_db -f /dumped_data_carwash.sql
